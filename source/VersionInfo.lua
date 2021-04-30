151f082ec57929ba85a92b17b8d1f771154b49a8
Silent <zdanio95@gmail.com>
2021-04-30 18:22:35 +0200

    First commit

---
+++ source/VersionInfo.lua
@@ -0,0 +1,8 @@
+defines { 
+    "rsc_FullName=\"SilentPatch for TOCA 2 Touring Cars\"",
+    "rsc_MinorVersion=0",
+    "rsc_RevisionID=1",
+    "rsc_BuildID=0",
+    "rsc_Copyright=\"2021\"",
+    "rsc_UpdateURL=\"https://github.com/CookiePLMonster/SilentPatchTOCA2/releases\""
+}
